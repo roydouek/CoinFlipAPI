9cf76fc59164d78c616de8b57a408f95506c77c9
Roee Douek <roydouek@gmail.com>
2021-12-17 17:12:15 +0200

    Head or tails added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const path = require('path');
+
 
 const indexRouter = require('./routes/index');
 
@@ -16,12 +18,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.set('view engine', 'pug')
 app.use('/', indexRouter);
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError.NotFound());
 });
+
+app.use('/images', express.static('images'));
+
+
 
 // pass any unhandled errors to the error handler
 app.use(errorHandler);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+function getRnd() {
+  return Math.random() > 0.5;
+}
+
 router.get('/', (req, res) => {
-  res.send({ message: 'Hello world' });
+  if (getRnd())
+    res.render('indexH')
+  else res.render('indexT')
 });
 
 module.exports = router;
